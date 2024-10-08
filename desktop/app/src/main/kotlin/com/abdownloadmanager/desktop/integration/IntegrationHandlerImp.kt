cf1e9b99a62ebb075f6e83081d2ae39929e5d3b4
Talha Ahmed <talha5389@mail.com>
2024-10-08 08:53:38 +0500

    feat: rest api to add silent downloads

--- desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/integration/IntegrationHandlerImp.kt
+++ desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/integration/IntegrationHandlerImp.kt
@@ -1,21 +1,62 @@
 package com.abdownloadmanager.desktop.integration
 
-import com.abdownloadmanager.integration.IntegrationHandler
 import com.abdownloadmanager.desktop.AppComponent
-import ir.amirab.downloader.downloaditem.DownloadCredentials
+import com.abdownloadmanager.desktop.repository.AppRepository
+import com.abdownloadmanager.desktop.utils.DownloadSystem
+import com.abdownloadmanager.integration.IntegrationHandler
 import com.abdownloadmanager.integration.NewDownloadInfoFromIntegration
+import com.abdownloadmanager.integration.NewDownloadTask
+import com.abdownloadmanager.integration.QueueModel
+import ir.amirab.downloader.downloaditem.DownloadCredentials
+import ir.amirab.downloader.downloaditem.DownloadItem
+import ir.amirab.downloader.queue.QueueManager
+import ir.amirab.downloader.utils.OnDuplicateStrategy
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class IntegrationHandlerImp: IntegrationHandler,KoinComponent{
+class IntegrationHandlerImp : IntegrationHandler, KoinComponent {
     val appComponent by inject<AppComponent>()
+    val downloadSystem by inject<DownloadSystem>()
+    val queueManager by inject<QueueManager>()
+    val appSettings by inject<AppRepository>()
     override suspend fun addDownload(list: List<NewDownloadInfoFromIntegration>) {
-        appComponent.openAddDownloadDialog(list.map {
-            DownloadCredentials(
-                link = it.link,
-                headers = it.headers,
-                downloadPage = it.downloadPage,
-            )
-        })
+        appComponent.openAddDownloadDialog(
+                list.map {
+                    DownloadCredentials(
+                            link = it.link,
+                            headers = it.headers,
+                            downloadPage = it.downloadPage,
+                    )
+                }
+        )
+    }
+    override fun listQueues(): List<QueueModel> {
+        return queueManager.getAll().map { downloadQueue ->
+            val queueModel = downloadQueue.getQueueModel()
+            QueueModel(id = queueModel.id, name = queueModel.name)
+        }
+    }
+    override suspend fun addDownloadTask(task: NewDownloadTask) {
+        val downloadItem =
+                DownloadItem(
+                        link = task.downloadSource.link,
+                        headers = task.downloadSource.headers,
+                        downloadPage = task.downloadSource.downloadPage,
+                        folder = task.folder ?: appSettings.saveLocation.value,
+                        id = -1,
+                        name = task.name ?: task.downloadSource.link.substringAfterLast("/"),
+                )
+        val id =
+                downloadSystem.addDownload(
+                        downloadItem,
+                        OnDuplicateStrategy.default(),
+                        task.queueId
+                )
+        if (task.queueId != null) {
+            val queue = queueManager.getQueue(task.queueId!!)
+            queue.start()
+        } else {
+            downloadSystem.manualResume(id)
+        }
     }
 }
--- integration/server/src/main/kotlin/com/abdownloadmanager/integration/IntegrationHandler.kt
+++ integration/server/src/main/kotlin/com/abdownloadmanager/integration/IntegrationHandler.kt
@@ -1,7 +1,7 @@
 package com.abdownloadmanager.integration
 
-interface IntegrationHandler{
-    suspend fun addDownload(
-        list: List<NewDownloadInfoFromIntegration>
-    )
+interface IntegrationHandler {
+    suspend fun addDownload(list: List<NewDownloadInfoFromIntegration>)
+    fun listQueues(): List<QueueModel>
+    suspend fun addDownloadTask(task: NewDownloadTask)
 }
