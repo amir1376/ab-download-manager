42e7310cee7af1614e05b0a3da84d7ab7ef83e96
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-01-17 01:53:52 +0330

    fix file checksum ui problem

--- desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/pages/filehash/FileChecksumWindow.kt
+++ desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/pages/filehash/FileChecksumWindow.kt
@@ -9,10 +9,13 @@ import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberWindowState
 import com.abdownloadmanager.desktop.AppComponent
 import com.abdownloadmanager.desktop.window.custom.CustomWindow
+import com.abdownloadmanager.shared.utils.mvi.HandleEffects
 import com.abdownloadmanager.shared.utils.ui.theme.LocalUiScale
 import com.arkivanov.decompose.Child
 import com.arkivanov.decompose.router.slot.ChildSlot
 import ir.amirab.util.desktop.screen.applyUiScale
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun FileChecksumWindow(
@@ -28,13 +31,22 @@ fun FileChecksumWindow(
     component: FileChecksumComponent
 ) {
     val uiScale = LocalUiScale.current
+    val state = rememberWindowState(
+        position = WindowPosition.Aligned(Alignment.Center),
+        size = DpSize(900.dp, 400.dp).applyUiScale(uiScale)
+    )
     CustomWindow(
-        state = rememberWindowState(
-            position = WindowPosition.Aligned(Alignment.Center),
-            size = DpSize(900.dp, 400.dp).applyUiScale(uiScale)
-        ),
+        state = state,
         onCloseRequest = component::onRequestClose
     ) {
+        HandleEffects(component) {
+            when (it) {
+                FileChecksumUiEffects.BringToFront -> {
+                    state.isMinimized = false
+                    window.toFront()
+                }
+            }
+        }
         FileChecksumPage(component)
     }
 }
