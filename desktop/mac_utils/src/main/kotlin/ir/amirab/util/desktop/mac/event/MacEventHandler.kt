a13a43a32a0934bda5c7888beaccca1fc214b7ed
Amirreza Gholami <129018081+Amirroid@users.noreply.github.com>
2025-04-04 03:40:13 +0330

    Override macOS About dialog (#476)
    * feat: override macOS About dialog

---
+++ desktop/mac_utils/src/main/kotlin/ir/amirab/util/desktop/mac/event/MacEventHandler.kt
@@ -0,0 +1,22 @@
+package ir.amirab.util.desktop.mac.event
+
+import java.awt.Desktop
+import java.awt.desktop.AppReopenedEvent
+import java.awt.desktop.AppReopenedListener
+
+object MacEventHandler {
+    fun configure(onClickIcon: () -> Unit, onAboutClick: () -> Unit) {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.APP_EVENT_REOPENED)) {
+            Desktop.getDesktop().apply {
+                addAppEventListener(object : AppReopenedListener {
+                    override fun appReopened(e: AppReopenedEvent?) {
+                        onClickIcon.invoke()
+                    }
+                })
+                if (isSupported(Desktop.Action.APP_ABOUT)) {
+                    setAboutHandler { onAboutClick.invoke() }
+                }
+            }
+        }
+    }
+}
