5eb4e59d3c627a588ae87aa65e7f31923bbb89a4
Amirreza Gholami <129018081+Amirroid@users.noreply.github.com>
2025-04-09 04:30:40 +0330

    Fix startup issues on macOS (#466)
    * Fix startup issues on macOS

--- shared/auto-start/src/main/kotlin/ir/amirab/util/startup/MacOSStartup.kt
+++ shared/auto-start/src/main/kotlin/ir/amirab/util/startup/MacOSStartup.kt
@@ -1,8 +1,6 @@
 package ir.amirab.util.startup
 
 import java.io.File
-import java.io.FileWriter
-import java.io.PrintWriter
 
 class MacOSStartup(
     name: String,
@@ -23,24 +21,51 @@ class MacOSStartup(
 
     @Throws(Exception::class)
     override fun install() {
-        val out = PrintWriter(FileWriter(getFile()))
-        out.println("<plist version=\"1.0\">")
-        out.println("<dict>")
-        out.println("\t<key>Label</key>")
-        out.println("\t<string>" + super.name + "</string>")
-        out.println("\t<key>ProgramArguments</key>")
-        out.println("\t<array>")
-        out.println("\t\t<string>" + getExecutableWithArgs() + "</string>")
-        out.println("\t</array>")
-        out.println("\t<key>RunAtLoad</key>")
-        out.println("\t<true/>")
-        out.println("</dict>")
-        out.println("</plist>")
-        out.close()
+        val file = getFile()
+
+        val plistContent = buildString {
+            appendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+            appendLine("<plist version=\"1.0\">")
+            appendLine("<dict>")
+            appendLine("\t<key>Label</key>")
+            appendLine("\t<string>${super.name}.startup</string>")
+            appendLine("\t<key>ProgramArguments</key>")
+            appendLine("\t<array>")
+            appendLine("\t\t<string>/usr/bin/open</string>")
+            appendLine("\t\t<string>-a</string>")
+            appendLine("\t\t<string>${super.path}</string>")
+            appendLine("\t\t<string>--args</string>")
+            args.forEach {
+                appendLine("\t\t<string>$it</string>")
+            }
+            appendLine("\t</array>")
+            appendLine("\t<key>RunAtLoad</key>")
+            appendLine("\t<true/>")
+            appendLine("\t<key>LimitLoadToSessionType</key>")
+            appendLine("\t<string>Aqua</string>")
+            appendLine("</dict>")
+            appendLine("</plist>")
+        }
+
+        file.bufferedWriter().use { it.write(plistContent) }
+
+        runLaunchctlCommand(LOAD_COMMAND, file.path)
     }
 
     override fun uninstall() {
-        getFile().delete()
+        val file = getFile()
+        if (file.exists()) {
+            runLaunchctlCommand(UNLOAD_COMMAND, file.path)
+            file.delete()
+        }
+    }
+
+
+    private fun runLaunchctlCommand(command: String, filePath: String) {
+        ProcessBuilder("launchctl", command, filePath)
+            .inheritIO()
+            .start()
+            .waitFor()
     }
 
     companion object {
@@ -55,5 +80,8 @@ class MacOSStartup(
 
                 return File("$home/Library/LaunchAgents/")
             }
+
+        private const val UNLOAD_COMMAND = "unload"
+        private const val LOAD_COMMAND = "load"
     }
 }
