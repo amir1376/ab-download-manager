88b27b3ea83e5003fe76ce232ad5848b36243ce4
Amirreza Gholami <129018081+Amirroid@users.noreply.github.com>
2025-04-25 17:02:06 +0330

    feat: add macOS settings menu handler with callback (#518)

--- desktop/mac_utils/src/main/kotlin/ir/amirab/util/desktop/mac/event/MacEventHandler.kt
+++ desktop/mac_utils/src/main/kotlin/ir/amirab/util/desktop/mac/event/MacEventHandler.kt
@@ -5,8 +5,14 @@ import java.awt.desktop.AppReopenedEvent
 import java.awt.desktop.AppReopenedListener
 
 object MacEventHandler {
-    fun configure(onClickIcon: () -> Unit, onAboutClick: () -> Unit) {
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.APP_EVENT_REOPENED)) {
+    fun configure(
+        onClickIcon: () -> Unit,
+        onAboutClick: () -> Unit,
+        onSettingsClick: () -> Unit
+    ) {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop()
+                .isSupported(Desktop.Action.APP_EVENT_REOPENED)
+        ) {
             Desktop.getDesktop().apply {
                 addAppEventListener(object : AppReopenedListener {
                     override fun appReopened(e: AppReopenedEvent?) {
@@ -15,6 +21,9 @@ object MacEventHandler {
                 })
                 if (isSupported(Desktop.Action.APP_ABOUT)) {
                     setAboutHandler { onAboutClick.invoke() }
+                }
+                if (isSupported(Desktop.Action.APP_PREFERENCES)) {
+                    setPreferencesHandler { onSettingsClick.invoke() }
                 }
             }
         }
