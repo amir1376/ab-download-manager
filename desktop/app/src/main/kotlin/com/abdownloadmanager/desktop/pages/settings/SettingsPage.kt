f663ac4e308062475e901595564402089ba168f2
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-05-07 08:27:08 +0330

    improve SettingsPage

--- desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/pages/settings/SettingsPage.kt
+++ desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/pages/settings/SettingsPage.kt
@@ -11,39 +11,47 @@ import com.abdownloadmanager.shared.utils.ui.myColors
 import com.abdownloadmanager.shared.ui.widget.Handle
 import com.abdownloadmanager.shared.ui.widget.Text
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.abdownloadmanager.resources.Res
 import com.abdownloadmanager.shared.utils.div
+import com.abdownloadmanager.shared.utils.ui.theme.myTextSizes
 import ir.amirab.util.compose.resources.myStringResource
+import ir.amirab.util.ifThen
 
 @Composable
 private fun SideBar(
     settingsComponent: SettingsComponent,
     modifier: Modifier = Modifier,
 ) {
+    val shape = RoundedCornerShape(6.dp)
     Column(
         modifier
-            .padding(horizontal = 12.dp)
             .fillMaxHeight()
+            .border(1.dp, myColors.surface / 0.5f, shape)
+            .clip(shape)
     ) {
 //        var searchText by remember { mutableStateOf("") }
-        Spacer(Modifier.height(12.dp))
 //        SearchBox(
 //            searchText,
 //            onTextChange = { searchText = it },
 //            modifier = Modifier.height(38.dp),
 //        )
-//        Spacer(Modifier.height(32.dp))
         for (i in settingsComponent.pages) {
             SideBarItem(
                 icon = i.icon,
@@ -59,42 +67,87 @@ private fun SideBar(
 
 @Composable
 private fun SideBarItem(icon: IconSource, name: String, isSelected: Boolean, onClick: () -> Unit) {
-    val shape = RoundedCornerShape(12.dp)
-    WithContentAlpha(if (isSelected) 1f else 0.75f) {
+    Box(
+        Modifier
+            .height(IntrinsicSize.Max)
+            .ifThen(isSelected) {
+                background(myColors.onBackground / 0.05f)
+            }
+            .selectable(
+                selected = isSelected,
+                onClick = onClick
+            )
+    ) {
         Row(
             Modifier
-                .fillMaxWidth()
-                .clip(shape)
-                .let {
-                    if (isSelected) {
-                        val selectionColor = myColors.onBackground
-                        it
-                            .border(
-                                1.dp,
-                                myColors.selectionGradient(0.10f, 0.05f, selectionColor),
-                                shape
-                            )
-                            .background(myColors.selectionGradient(0.15f, 0f, selectionColor))
-                    } else it
-                }
-                .onClick {
-                    onClick()
-                }
-                .padding(8.dp),
+                .padding(vertical = 8.dp)
+                .padding(start = 16.dp)
+                .padding(end = 2.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            MyIcon(icon, null, Modifier.size(16.dp))
-            Spacer(Modifier.width(8.dp))
-            Text(
-                name,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+            WithContentAlpha(if (isSelected) 1f else 0.75f) {
+                MyIcon(
+                    icon,
+                    null,
+                    Modifier.size(16.dp)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(
+                    name,
+                    Modifier.weight(1f),
+                    fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+                    fontSize = myTextSizes.lg,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+            }
+        }
+        AnimatedVisibility(
+            isSelected,
+            modifier = Modifier
+                .align(Alignment.CenterStart),
+            enter = scaleIn(),
+            exit = scaleOut(),
+        ) {
+            Spacer(
+                Modifier
+                    .height(16.dp)
+                    .width(3.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 0.dp,
+                            bottomStart = 0.dp,
+                            bottomEnd = 12.dp,
+                            topEnd = 12.dp,
+                        )
+                    )
+                    .background(myColors.primary)
             )
         }
+        if (isSelected) {
+            listOf(
+                Alignment.TopCenter,
+                Alignment.BottomCenter,
+            ).forEach {
+                Spacer(
+                    Modifier
+                        .align(it)
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(
+                            Brush.horizontalGradient(
+                                listOf(
+                                    Color.Transparent,
+                                    myColors.onBackground / 0.1f,
+                                    myColors.onBackground / 0.1f,
+                                    Color.Transparent,
+                                )
+                            )
+                        )
+                )
+            }
+        }
     }
-
-
 }
 
 @Composable
@@ -105,47 +158,64 @@ fun SettingsPage(
     WindowTitle(myStringResource(Res.string.settings))
 //    WindowIcon(MyIcons.settings)
     WindowIcon(MyIcons.appIcon)
-    Row {
-        var sideBarWidth by remember { mutableStateOf(250.dp) }
-        SideBar(settingsComponent, Modifier.fillMaxHeight().width(sideBarWidth))
-        val currentConfigurables = settingsComponent.configurables
-        Handle(
-            Modifier.width(5.dp).fillMaxHeight(),
-            orientation = Orientation.Horizontal
-        ) {
-            sideBarWidth = (sideBarWidth + it).coerceIn(150.dp..300.dp)
-        }
-        AnimatedContent(currentConfigurables) { configurables ->
-            val scrollState = rememberScrollState()
-            val scrollbarAdapter = rememberScrollbarAdapter(scrollState)
-            Box {
-                Column(
-                    Modifier
-                        .verticalScroll(scrollState)
-                        .padding(16.dp)
-                ) {
-                    for (cfg in configurables) {
-                        Box(
-                            Modifier
-                                .background(myColors.surface / 50)
-                        ) {
-                            RenderConfigurable(cfg, Modifier.padding(vertical = 16.dp, horizontal = 32.dp))
-                        }
-                        Spacer(Modifier.height(1.dp))
+    Column {
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(myColors.surface)
+        )
+        Row {
+            var sideBarWidth by remember { mutableStateOf(250.dp) }
+            SideBar(
+                settingsComponent,
+                Modifier
+                    .fillMaxHeight()
+                    .width(sideBarWidth)
+                    .padding(8.dp)
+            )
+            val currentConfigurables = settingsComponent.configurables
+            Handle(
+                Modifier.width(5.dp).fillMaxHeight(),
+                orientation = Orientation.Horizontal
+            ) {
+                sideBarWidth = (sideBarWidth + it).coerceIn(150.dp..300.dp)
+            }
+            AnimatedContent(currentConfigurables) { configurables ->
+                val scrollState = rememberScrollState()
+                val scrollbarAdapter = rememberScrollbarAdapter(scrollState)
+                Box {
+                    Column(
+                        Modifier
+                            .verticalScroll(scrollState)
+                            .padding(
+                                horizontal = 16.dp,
+                                vertical = 8.dp
+                            )
+                    ) {
+                        for (cfg in configurables) {
+                            Box(
+                                Modifier
+                                    .background(myColors.surface / 50)
+                            ) {
+                                RenderConfigurable(cfg, Modifier.padding(vertical = 16.dp, horizontal = 32.dp))
+                            }
+                            Spacer(Modifier.height(1.dp))
 
 //                    Divider()
+                        }
                     }
+                    VerticalScrollbar(
+                        adapter = scrollbarAdapter,
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                            .padding(vertical = 16.dp)
+                            .padding(end = 2.dp),
+                    )
                 }
-                VerticalScrollbar(
-                    adapter = scrollbarAdapter,
-                    modifier = Modifier
-                        .align(Alignment.CenterEnd)
-                        .padding(vertical = 16.dp)
-                        .padding(end = 2.dp),
-                )
             }
-        }
 
+        }
     }
 }
 
