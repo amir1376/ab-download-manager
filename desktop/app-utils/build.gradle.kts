edbf2500e6b0163f8c76f94bd52c55a4b5601405
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-05-11 15:35:27 +0330

    move dependency to version catalogs

--- desktop/app-utils/build.gradle.kts
+++ desktop/app-utils/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     api(project(":desktop:shared"))
     api(project(":shared:app"))
-    implementation("org.jetbrains.runtime:jbr-api:1.5.0")
+    implementation(libs.jbrApi)
 }
