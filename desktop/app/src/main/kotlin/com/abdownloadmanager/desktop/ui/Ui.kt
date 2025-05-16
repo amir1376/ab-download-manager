e60495bad18d7cd9373646b818e9134e44362ffc
Amirreza Gholami <129018081+Amirroid@users.noreply.github.com>
2025-05-16 10:45:14 +0330

    fix: override quit handler (#596)
    * fix: override quit handler

--- desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/ui/Ui.kt
+++ desktop/app/src/main/kotlin/com/abdownloadmanager/desktop/ui/Ui.kt
@@ -72,7 +72,10 @@ object Ui : KoinComponent {
                 onAboutClick = {
                     appComponent.showAboutPage.value = true
                 },
-                onSettingsClick = appComponent::openSettings
+                onSettingsClick = appComponent::openSettings,
+                onQuit = {
+                    scope.launch { appComponent.requestExitApp() }
+                }
             )
         }
         application {
--- desktop/mac_utils/src/main/kotlin/ir/amirab/util/desktop/mac/event/MacEventHandler.kt
+++ desktop/mac_utils/src/main/kotlin/ir/amirab/util/desktop/mac/event/MacEventHandler.kt
@@ -3,12 +3,16 @@ package ir.amirab.util.desktop.mac.event
 import java.awt.Desktop
 import java.awt.desktop.AppReopenedEvent
 import java.awt.desktop.AppReopenedListener
+import java.awt.desktop.QuitEvent
+import java.awt.desktop.QuitHandler
+import java.awt.desktop.QuitResponse
 
 object MacEventHandler {
     fun configure(
         onClickIcon: () -> Unit,
         onAboutClick: () -> Unit,
-        onSettingsClick: () -> Unit
+        onSettingsClick: () -> Unit,
+        onQuit: () -> Unit,
     ) {
         if (Desktop.isDesktopSupported() && Desktop.getDesktop()
                 .isSupported(Desktop.Action.APP_EVENT_REOPENED)
@@ -24,6 +28,12 @@ object MacEventHandler {
                 }
                 if (isSupported(Desktop.Action.APP_PREFERENCES)) {
                     setPreferencesHandler { onSettingsClick.invoke() }
+                }
+                if (isSupported(Desktop.Action.APP_QUIT_HANDLER)) {
+                    setQuitHandler { _, response ->
+                        response.cancelQuit()
+                        onQuit.invoke()
+                    }
                 }
             }
         }
