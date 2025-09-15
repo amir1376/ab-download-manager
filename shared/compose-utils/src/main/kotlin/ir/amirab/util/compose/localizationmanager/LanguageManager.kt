dccc8046d04755f6a963c1a5769502d0dbcf53fb
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-09-16 02:33:37 +0330

    update RTL language list

--- shared/compose-utils/src/main/kotlin/ir/amirab/util/compose/localizationmanager/LanguageManager.kt
+++ shared/compose-utils/src/main/kotlin/ir/amirab/util/compose/localizationmanager/LanguageManager.kt
@@ -175,7 +175,7 @@ class LanguageManager(
             )
         }
 
-        private val rtlLanguages = arrayOf("ar", "bqi", "fa", "he", "iw", "ji", "ur", "yi")
+        private val rtlLanguages = arrayOf("ar", "bqi", "ckb", "fa", "he", "iw", "ji", "ur", "yi")
 
         private fun extractLanguageCodeAndCountryCodeFromFileName(name: String): MyLocale? {
             return name
