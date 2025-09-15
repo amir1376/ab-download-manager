a619a998e82e3923a6447bbef1bd488f85e6bf14
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-09-15 08:13:04 +0330

    remove unnecessary buffer from the source and cleanup the code

--- downloader/core/src/main/kotlin/ir/amirab/downloader/connection/Connection.kt
+++ downloader/core/src/main/kotlin/ir/amirab/downloader/connection/Connection.kt
@@ -1,11 +1,11 @@
 package ir.amirab.downloader.connection
 
 import ir.amirab.downloader.connection.response.ResponseInfo
-import okio.BufferedSource
+import okio.Source
 import java.io.Closeable
 
 data class Connection(
-    val source: BufferedSource,
+    val source: Source,
     val contentLength: Long,
     val closeable: Closeable,
     val responseInfo: ResponseInfo,
