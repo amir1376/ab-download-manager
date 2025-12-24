06da0231aed4026457472a2d7f1968474ba67974
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2025-12-25 02:21:08 +0330

    improve update component notifications

--- shared/app/src/commonMain/kotlin/com/abdownloadmanager/shared/pages/updater/UpdateComponent.kt
+++ shared/app/src/commonMain/kotlin/com/abdownloadmanager/shared/pages/updater/UpdateComponent.kt
@@ -1,24 +1,30 @@
 package com.abdownloadmanager.shared.pages.updater
 
+import com.abdownloadmanager.UpdateCheckStatus
 import com.abdownloadmanager.shared.util.AppVersion
 import com.abdownloadmanager.shared.util.BaseComponent
 import com.abdownloadmanager.UpdateManager
 import com.abdownloadmanager.resources.Res
 import com.abdownloadmanager.shared.pagemanager.NotificationSender
 import com.abdownloadmanager.shared.ui.widget.MessageDialogType
+import com.abdownloadmanager.shared.util.mvi.ContainsEffects
+import com.abdownloadmanager.shared.util.mvi.supportEffects
 import com.arkivanov.decompose.ComponentContext
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 import ir.amirab.util.compose.asStringSource
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class UpdateComponent(
     ctx: ComponentContext,
     private val notificationSender: NotificationSender,
     private val updateManager: UpdateManager,
 ) : BaseComponent(ctx),
+    ContainsEffects<UpdateComponent.Effects> by supportEffects(),
     KoinComponent {
 
     fun isUpdateSupported(): Boolean {
@@ -30,7 +36,7 @@ class UpdateComponent(
     val newVersionData = updateManager.newVersionData
     private var updateApplierJob: Job? = null
 
-    var updateCheckStatus = updateManager.updateCheckStatus
+    val updateCheckStatus = updateManager.updateCheckStatus
 
     fun performUpdate() {
         updateApplierJob?.cancel()
@@ -58,15 +64,47 @@ class UpdateComponent(
 
     fun requestCheckForUpdate() {
         scope.launch {
-            updateManager
-                .checkForUpdate()
-                ?.let {
-                    showNewUpdate()
-                }
+            updateManager.checkForUpdate()
         }
     }
 
     fun requestClose() {
         showNewUpdate.update { false }
+    }
+
+    init {
+        updateCheckStatus
+            .drop(1) // state flow
+            .onEach {
+                when (it) {
+                    UpdateCheckStatus.Checking -> {
+                        sendEffect(Effects.CheckingForUpdate)
+                    }
+
+                    is UpdateCheckStatus.Error -> {
+                        sendEffect(Effects.Error(it.e))
+                    }
+
+                    UpdateCheckStatus.NewUpdate -> {
+                        sendEffect(Effects.NewUpdate)
+                        showNewUpdate()
+                    }
+
+                    UpdateCheckStatus.NoUpdate -> {
+                        sendEffect(Effects.NoUpdate)
+                    }
+
+                    UpdateCheckStatus.IDLE -> {
+
+                    }
+                }
+            }.launchIn(scope)
+    }
+
+    sealed interface Effects {
+        data object CheckingForUpdate : Effects
+        data object NoUpdate : Effects
+        data object NewUpdate : Effects
+        data class Error(val throwable: Throwable) : Effects
     }
 }
