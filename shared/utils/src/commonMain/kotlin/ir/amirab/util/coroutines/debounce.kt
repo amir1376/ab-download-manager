36d26f12a2740ee4b88d62245afee9c48b8bed8e
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2026-02-12 16:19:25 +0330

    improve queue logic

--- shared/utils/src/commonMain/kotlin/ir/amirab/util/coroutines/debounce.kt
+++ shared/utils/src/commonMain/kotlin/ir/amirab/util/coroutines/debounce.kt
@@ -1,5 +1,6 @@
 package ir.amirab.util.coroutines
 
+import ir.amirab.util.ValueHolder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -15,6 +16,29 @@ fun CoroutineScope.debounce(
         lastRun = launch {
             delay(delayMillis)
             fn.invoke()
+        }
+    }
+}
+
+fun <T> CoroutineScope.debounce(
+    fn: (T) -> Unit,
+    delayMillis: Long,
+    previousValueMerge: ((previous: T, current: T) -> T)? = null,
+): (T) -> Unit {
+    var lastRun: Job? = null
+    val previousValueHolder = ValueHolder(null as T?)
+    return { v ->
+        val previousValue = previousValueHolder.value
+        val param = if (previousValueMerge != null && previousValue != null) {
+            previousValueMerge(previousValue, v)
+        } else {
+            v
+        }
+        previousValueHolder.value = v
+        lastRun?.cancel()
+        lastRun = launch {
+            delay(delayMillis)
+            fn.invoke(param)
         }
     }
 }
