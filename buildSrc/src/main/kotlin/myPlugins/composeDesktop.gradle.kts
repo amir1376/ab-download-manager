f3b15e68b37ad470ee6ee8187ca50d0f2b594512
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2026-07-14 10:33:24 +0330

    directly add skiko dependency (#1321)

--- buildSrc/src/main/kotlin/myPlugins/composeDesktop.gradle.kts
+++ buildSrc/src/main/kotlin/myPlugins/composeDesktop.gradle.kts
@@ -5,8 +5,40 @@ plugins {
     id("myPlugins.composeBase")
 }
 
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
 dependencies {
-    api(compose.desktop.currentOs){
-        exclude("org.jetbrains.compose.material")
+    // Manually add these dependencies to prevent material dependencies from sneaking in!
+    val skikoVersion = libs.findVersion("skiko").get().requiredVersion
+    implementation(SkikoRuntimeUtil.dependencyForCurrentOS(skikoVersion))
+    implementation(libs.findLibrary("compose.runtime").get())
+    implementation(libs.findLibrary("compose.foundation").get())
+    implementation(libs.findLibrary("compose.ui").get())
+}
+
+object SkikoRuntimeUtil {
+    /**
+     * Don't use Platform.asDesktop() as I (or them) may change these names
+     * https://github.com/JetBrains/skiko
+     */
+    private fun getCurrentOS(): String {
+        val osName = System.getProperty("os.name")
+        val targetOs = when {
+            osName == "Mac OS X" -> "macos"
+            osName.startsWith("Win") -> "windows"
+            osName.startsWith("Linux") -> "linux"
+            else -> error("Unsupported OS: $osName")
+        }
+
+        val osArch = System.getProperty("os.arch")
+        val targetArch = when (osArch) {
+            "x86_64", "amd64" -> "x64"
+            "aarch64" -> "arm64"
+            else -> error("Unsupported arch: $osArch")
+        }
+        return "$targetOs-$targetArch"
+    }
+    fun dependencyForCurrentOS(version: String): String {
+        return "org.jetbrains.skiko:skiko-awt-runtime-${getCurrentOS()}:$version"
     }
 }
